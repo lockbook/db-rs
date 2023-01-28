c84a5720653f8afc0be6941c52580117be375a3c
Parth <parth@mehrotra.me>
2023-01-28 06:31:27 +0100

    cleanup macro

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -23,21 +23,19 @@ pub fn db(input: TokenStream) -> TokenStream {
         _ => panic!("db schema must be a struct"),
     };
 
-    let idents1 = tables.iter().map(|table| table.ident.clone().unwrap());
-    let idents2 = tables.iter().map(|table| table.ident.clone().unwrap());
-    let idents3 = tables.iter().map(|table| table.ident.clone().unwrap());
-    let idents4 = tables
+    let idents: &Vec<&Ident> = &tables
         .iter()
-        .map(|table| table.ident.clone().unwrap())
+        .map(|table| table.ident.as_ref().unwrap())
+        .collect();
+
+    let last = &tables
+        .iter()
+        .map(|table| table.ident.as_ref().unwrap())
         .last()
         .unwrap();
-    let types = tables.iter().map(|table| table.ty.clone());
-    let ids: Vec<u8> = vec![0, 1, 2];
-    // let ids: Vec<LitInt> = (0..idents.len())
-    //     .into_iter()
-    //     .map(|i| format!("{}_u8", i))
-    //     .map(|u| LitInt::new(&u, ident.span()))
-    //     .collect();
+
+    let types = tables.iter().map(|table| &table.ty);
+    let ids: Vec<u8> = (0..idents.len() as u8).into_iter().collect();
 
     let output = quote! {
         use db_rs::logger::Logger;
@@ -49,22 +47,22 @@ pub fn db(input: TokenStream) -> TokenStream {
                 let log = Logger::init(config);
                 let log_entries = log.get_entries();
 
-                #( let mut #idents1 = <#types>::init(#ids, log.clone()); )*
+                #( let mut #idents = <#types>::init(#ids, log.clone()); )*
 
                 for entry in log_entries {
                     match entry.table_id {
-                        #( #ids => #idents2.handle_event(entry.bytes), )*
+                        #( #ids => #idents.handle_event(entry.bytes), )*
                         _ => todo!()
                     }
                 }
 
                 Self {
-                    #( #idents3, )*
+                    #( #idents, )*
                 }
             }
 
             fn get_logger(&mut self) -> &mut Logger {
-                &mut self.#idents4.logger
+                &mut self.#last.logger
             }
         }
     };
--- example/src/main.rs
+++ example/src/main.rs
@@ -13,29 +13,3 @@ struct SampleSchemaV1 {
 fn main() {
     let a = SampleSchemaV1::init(Config::default());
 }
-//
-// impl Db for SampleSchemaV1 {
-//     fn init(location: Config) -> Self {
-//         let log = Logger::init(location);
-//         let log_entries = log.get_entries();
-//
-//         let mut table_0 = LookupTable::<String, String, Bincode>::init(0, log.clone());
-//         let mut table_1 = Single::<bool, Bincode>::init(1, log.clone());
-//
-//         for entry in log_entries {
-//             match entry.table_id {
-//                 0 => table_0.handle_event(entry.bytes),
-//                 1 => table_1.handle_event(entry.bytes),
-//                 _ => todo!(),
-//             }
-//         }
-//         Self {
-//             names: table_0,
-//             is_good: table_1,
-//         }
-//     }
-//
-//     fn get_logger(&mut self) -> &mut Logger {
-//         &mut self.names.logger
-//     }
-// }
