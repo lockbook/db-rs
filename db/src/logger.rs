25faaf35e3cee4c1fda83348301e11808a1213fb
Parth <parth@mehrotra.me>
2023-02-02 18:08:12 -0500

    cleanup init

--- db/src/logger.rs
+++ db/src/logger.rs
@@ -38,8 +38,11 @@ impl Logger {
         let tx_data = None;
         let current_txs = 0;
 
-        let inner = LoggerInner { file, config, incomplete_write, tx_data, current_txs };
-        let inner = Rc::new(RefCell::new(inner));
+        let inner = Rc::new(
+            RefCell::new(
+                LoggerInner { file, config, incomplete_write, tx_data, current_txs }
+            )
+        );
 
         Ok(Self { inner })
     }
