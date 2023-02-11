52b7bfb76d396a225129cbe01982450bd3bb5331
Parth <parth@mehrotra.me>
2023-02-11 17:08:37 -0500

    make db send
    * rc -> arc, refcell -> mutex
    
    * 0.1.10
    
    * fix tests
    
    * fix tests

--- db/src/lookup.rs
+++ db/src/lookup.rs
@@ -63,8 +63,8 @@ where
 
 impl<K, V> LookupTable<K, V>
 where
-    K: Hash + Eq + Serialize + DeserializeOwned,
-    V: Serialize + DeserializeOwned,
+    K: Hash + Eq + Serialize,
+    V: Serialize,
 {
     pub fn insert(&mut self, key: K, value: V) -> DbResult<Option<V>> {
         let log_entry = LogEntry::Insert(&key, &value);
