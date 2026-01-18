3bff942701dc8641d2ec21abdcb52553e118e256
ad-tra <aew31gcmuy@gmail.com>
2026-01-17 20:01:57 -0500

    add a nested hashmap

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -109,6 +109,7 @@ pub use crate::logger::TxHandle;
 pub use crate::list::List;
 pub use crate::lookup::LookupTable;
 pub use crate::lookup_list::LookupList;
+pub use crate::lookup_map::LookupMap;
 pub use crate::lookup_set::LookupSet;
 pub use crate::single::Single;
 
@@ -120,6 +121,7 @@ pub mod list;
 pub mod logger;
 pub mod lookup;
 pub mod lookup_list;
+pub mod lookup_map;
 pub mod lookup_set;
 pub mod single;
 pub mod table;
